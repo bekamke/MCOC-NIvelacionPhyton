ed79610bd6d1169b1407417f6fcecda291705645
bekamke <53590243+bekamke@users.noreply.github.com>
2019-08-19 23:32:47 -0400

    Add files via upload

---
+++ 19082019/Ejemplos mezclados videos.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 22:58:15 2019
+
+@author: Roberto Kamke
+"""
+#Estoy mezclando todos los ejemplos que estan saliendo en el video
+
+a = 1 # Se le asigna el valor a una variable
+
+b = 2 # Se le asigna el valor a una variable
+
+c = 5 # Se le asigna el valor a una variable
+
+d = 4 # Se le asigna el valor a una variable
+
+e = 7 # Se le asigna el valor a una variable
+
+f = 7 # Se le asigna el valor a una variable
+
+if a < b: # Se asigna la variable IF para poner una condición
+
+    print "a es menor que b" #Si es que la condicion del IF se cumple se va a imprimir lo que sale siguiente al print
+
+elif a == b:
+    
+    print "a es igual a b"
+
+    
+else: # Se asigna la variable ELSE para poner una "condicion" si es que la del IF no se cumple
+
+    print "b es mayor que a" #Si es que no se cumple IF y se cumple la condicion del ELSE se va a imprimir lo que sale siguiente al print
+ 
+#En este caso veremos como funciona el else
+if c < d:
+    
+    print "c es menor que d"
+    
+elif c == d:
+    
+    print "c es igual a d"    
+    
+else: 
+    print" d es mayor que c"
+    
+
+if e < f:
+    
+    print "e es menor que f"
+    
+elif e == f:
+    
+    print "e es igual a f"    
+    
+else: 
+    print" e es mayor que f"
+
+    
